fc7a7dc596199767ffb4abe56b873b10c459f373
YoinkedYoink <87105685+YoinkedYoink@users.noreply.github.com>
2022-12-18 03:06:39 +0000

    Fixed ONYX Disabler, added the server crasher
    Just fixed the disabler :DDDDDDDDDDDDD
    Added the server crasher thing :))))))

--- Developing/Airsoft_CenterV1.lua
+++ Developing/Airsoft_CenterV1.lua
@@ -58,7 +58,7 @@ end)
 
 local CFrameFly = SecondPage.AddToggle("CFrameFly", false, function(Value)
     _G.CFrameFly = Value
-    loadstring(game:HttpGet"https://pastebin.com/raw/deJdYep7")()
+    loadstring(game:HttpGet"https://pastebin.com/raw/CUTcjND0")()
 end)
 
 
@@ -69,13 +69,13 @@ end)
 
 local ThirdPage = MainUI.AddPage("Visuals")
 
-local FOVSlider = ThirdPage.AddSlider("FOV(patched)", {Min = 1, Max = 120, Def = 90}, function(Value)
+local FOVSlider = ThirdPage.AddSlider("FOV(Use Disabler)", {Min = 1, Max = 120, Def = 90}, function(Value)
     FOV = Value
     rconsoleprint("FOV Value is "..FOV)
     rconsoleprint("\n")
 end)
 
-local FOVButton = ThirdPage.AddToggle("Set FOV(patched)", false, function(Value)
+local FOVButton = ThirdPage.AddToggle("Set FOV(Use Disabler)", false, function(Value)
     _G.FOVSet = Value
     rconsoleprint("Set FOV to "..FOV)
     rconsoleprint("\n")
@@ -223,13 +223,13 @@ end)
 
 local SixthPage = MainUI.AddPage("Old stuff")
 
-local HipHeightSlider = SixthPage.AddSlider("Hip Height(patched for now)", {Min = 0, Max = 50, Def = 0}, function(Value)
+local HipHeightSlider = SixthPage.AddSlider("Hip Height(Use Disabler)", {Min = 0, Max = 50, Def = 0}, function(Value)
     HipHeight = Value
     rconsoleprint(HipHeight.." Slider HipHeight")
     rconsoleprint("\n")
 end)
 
-local HipHeightButton = SixthPage.AddButton("Set Hip Height(patched for now)", function()
+local HipHeightButton = SixthPage.AddButton("Set Hip Height(Use Disabler)", function()
     rconsoleprint(HipHeight.." Set HipHeight")
     rconsoleprint("\n")
     Player.Character.Humanoid.HipHeight = HipHeight
@@ -260,21 +260,15 @@ local HahaFunny = SeventhPage.AddToggle("Do a funny", false, function(Value)
     end
 end)
 
-local ONYXDISABLER = SeventhPage.AddButton("ONYX Disabler(patched)", function()
-    loadstring(game:HttpGet"https://pastebin.com/64K0qYfG")()
+local ONYXDISABLER = SeventhPage.AddButton("ONYX Disabler", function()
+    Player.Character.Humanoid.Name = "LMAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
     rconsolewarn("Disabler Used!")
 end)
 
-local ServerLagger = SeventhPage.AddToggle("Increase receving and ping a bit(AK47)", false, function(Value)
+local ServerLagger = SeventhPage.AddToggle("Server Crasher/Lagger", false, function(Value)
     _G.Lag = Value
     while _G.Lag do
-        userdata_1 = game:GetService("ReplicatedStorage").ONYX.Guns.AK47;
-        bool_1 = true;
-        game:GetService("ReplicatedStorage").ONYX.Remotes.GunSetup:FireServer(userdata_1, bool_1);
-        wait(0.01)
-        userdata_1 = game:GetService("ReplicatedStorage").ONYX.Guns.AK47;
-        bool_1 = false;
-        game:GetService("ReplicatedStorage").ONYX.Remotes.GunSetup:FireServer(userdata_1, bool_1);
-        wait()
+        task.wait()
+        game:GetService("ReplicatedStorage").ONYX.Remotes.GunSetup:FireServer(game:GetService("ReplicatedStorage").ONYX.Guns.AK47, true);
     end
 end)
